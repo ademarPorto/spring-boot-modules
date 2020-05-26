039acdc232735c93d731e845dd52fd580280e37f
ademarporto <ademar.dealmeidaporto@leaseplan.com>
2020-05-26 22:39:56 +0200

    First Commit

---
+++ spring-security-helper/src/main/java/com/ademarporto/springsecurityhelper/SpringSecurityHelperApplication.java
@@ -0,0 +1,12 @@
+package com.ademarporto.springsecurityhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityHelperApplication.class, args);
+	}
+}
---
+++ spring-security-helper/src/test/java/com/ademarporto/springsecurityhelper/SpringSecurityHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ademarporto.springsecurityhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
